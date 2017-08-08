1dd95a947134bf9c0e66aeb3fa7d4d2766fb2d8b
doforfutrue <794539959@qq.com>
2017-08-08 17:57:48 +0800

    1.0

---
+++ ABPProject.Application/CommonDto/KeyValuesParam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABPProject.CommonDto
+{
+    public class KeyValuesParam<TKey,TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue[] Values { get; set; }
+
+    }
+}
